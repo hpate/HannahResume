afbec8ac34464c7210c9c0c0c1ce340edabf58df
hpate <hpate@mail.usf.edu>
2015-08-17 11:51:17 -0400

    App Completed

--- app/src/main/java/edu/usf/myweb/hpate/hannahresume/Resume.java
+++ app/src/main/java/edu/usf/myweb/hpate/hannahresume/Resume.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
 
 public class Resume extends AppCompatActivity {
 
@@ -11,6 +15,17 @@ public class Resume extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        Button button = (Button) findViewById(R.id.linkedin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri url = Uri.parse("https://www.linkedin.com/in/thehannah");
+                Intent goToLinkedin = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(goToLinkedin);
+            }
+        });
     }
 
     @Override
